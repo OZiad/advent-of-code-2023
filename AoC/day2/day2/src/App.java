9f842ed8e9c9c91c8537eb660bc5623447a5d7c1
Ziad <omar.ziad1@outlook.com>
2023-12-02 02:12:33 -0500

    Create Main function

---
+++ AoC/day2/day2/src/App.java
@@ -0,0 +1,8 @@
+// https://adventofcode.com/2023/day/2
+// 12 red cubes, 13 green cubes, and 14 blue cubes
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
