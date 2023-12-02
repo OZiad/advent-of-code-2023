b461a598edae129627a857a16d4d7dfbce5b2404
Omar Ziad <omar.ziad1@outlook.com>
2023-12-01 20:01:02 -0500

    Solution for part 1 & 2

---
+++ AoC/day1/src/App.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String filePath = "input.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            int cumulativeSum = 0;
+            int lineNumber = 0;
+            while ((line = br.readLine()) != null) {
+                Integer result = calibrationValue(line);
+
+                if (result != null) {
+                    System.out.println(
+                            "Calibration value for line: number: " + ++lineNumber + "line: " + line + " is " + result);
+                    cumulativeSum += result;
+                    System.out.println("Total Sum is: " + cumulativeSum);
+                } else {
+                    System.out.println("No digits found in the line: " + line);
+                }
+            }
+            System.out.println("Total Sum: " + cumulativeSum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String replaceLetteredNumbers(String line) {
+        String[] searchList = new String[] { "oneight", "twone", "eightwo", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine" };
+        String[] replacementList = new String[] { "18", "21", "82", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        return StringUtils.replaceEach(line, searchList, replacementList);
+    }
+
+    public static Integer calibrationValue(String line) {
+        line = replaceLetteredNumbers(line);
+        int left = 0;
+        int right = line.length() - 1;
+        while (left <= right) {
+            if (!Character.isDigit(line.charAt(left))) {
+                left++;
+            }
+            if (!Character.isDigit(line.charAt(right))) {
+                right--;
+            }
+            if (left <= right && Character.isDigit(line.charAt(left)) && Character.isDigit(line.charAt(right))) {
+                String result = line.charAt(left) + "" + line.charAt(right);
+                return Integer.parseInt(result);
+            }
+        }
+
+        return null;
+    }
+}
