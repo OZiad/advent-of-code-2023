497385886f6c4f367f34c7b0908c8e36183f1322
Ziad <omar.ziad1@outlook.com>
2023-12-04 21:40:36 -0500

    Open file and read lines

--- AoC/day3/main.cpp
+++ AoC/day3/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
+using std::cout, std::string;
 int main()
 {
+    string input_file_path = "input.txt";
+    string line;
+    std::ifstream input_file(input_file_path);
+    if (input_file.is_open())
+    {
+        while (getline(input_file, line))
+        {
+            cout << line << '\n';
+        }
+        input_file.close();
+    }
+
+    else
+        cout << "Unable to open file";
     return 0;
 }
