932706eb6ecc6e2d7d7ff5cfc70b512d18b85ae1
Ziad <omar.ziad1@outlook.com>
2023-12-15 19:49:14 -0500

    Create skeleton for driver

---
+++ AoC/day4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+using std::cout, std::string, std::vector, std::endl, std::unordered_set;
+
+int main()
+{
+
+    cout << "hello world!" << endl;
+    return 0;
+}
